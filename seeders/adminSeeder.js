dda4b543889507a0667201fd019dd741c1a388e6
Sebasan22 <callofsebas@hotmail.com>
2023-03-23 15:19:37 -0300

    Update adminSeeder.js

--- seeders/adminSeeder.js
+++ seeders/adminSeeder.js
@@ -1,17 +1,17 @@
 const { Admin } = require("../models");
-
-const admins = [
-  {
-    firstname: "Admin",
-    lastname: "Admin",
-    email: "admin@hotmail.com",
-    password: "1234",
-    address: { location: null, tel: null },
-    avatar: null,
-  },
-];
+const bcrypt = require("bcryptjs");
 
 module.exports = async () => {
+  const admins = [
+    {
+      firstname: "Admin",
+      lastname: "Admin",
+      email: "admin@hotmail.com",
+      password: await bcrypt.hash("1234", 8),
+      address: { location: null, tel: null },
+      avatar: null,
+    },
+  ];
   await Admin.bulkCreate(admins);
   console.log("[Database] Se corrió el seeder de Admins.");
 };
