b72f9ffef98912c3620a26ba325a116a755e19a1
Sebasan22 <callofsebas@hotmail.com>
2023-03-23 15:15:49 -0300

    Update userSeeder.js

--- seeders/userSeeder.js
+++ seeders/userSeeder.js
@@ -1,25 +1,25 @@
 const { User } = require("../models");
-
-const users = [
-  {
-    firstname: "Seba",
-    lastname: "Yaben",
-    email: "seba@hotmail.com",
-    password: "1234",
-    address: { location: "Villa Dolores", tel: "9999999" },
-    avatar: null,
-  },
-  {
-    firstname: "Cristiano",
-    lastname: "Ronaldo",
-    email: "cris@hotmail.com",
-    password: "1234",
-    address: { location: "Peñarol", tel: "555222333" },
-    avatar: "https://www.fichajes.com/build/images/player-covers/cristiano-ronaldo.352c95f5.jpg",
-  },
-];
+const bcrypt = require("bcryptjs");
 
 module.exports = async () => {
+  const users = [
+    {
+      firstname: "Seba",
+      lastname: "Yaben",
+      email: "seba@hotmail.com",
+      password: await bcrypt.hash("1234", 8),
+      address: { location: "Villa Dolores", tel: "9999999" },
+      avatar: null,
+    },
+    {
+      firstname: "Cristiano",
+      lastname: "Ronaldo",
+      email: "cris@hotmail.com",
+      password: await bcrypt.hash("1234", 8),
+      address: { location: "Peñarol", tel: "555222333" },
+      avatar: "https://www.fichajes.com/build/images/player-covers/cristiano-ronaldo.352c95f5.jpg",
+    },
+  ];
   await User.bulkCreate(users);
   console.log("[Database] Se corrió el seeder de Users.");
 };
