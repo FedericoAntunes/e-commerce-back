27209f3db6cbda43239ab1894583219aa86c6798
FedericoAntunes <91098946+FedericoAntunes@users.noreply.github.com>
2023-03-26 01:34:54 -0300

    Added some companies to companySeeder

--- seeders/companySeeder.js
+++ seeders/companySeeder.js
@@ -13,7 +13,7 @@ const companies = [
   {
     name: "La Pasiva",
     slug: sluggy("La Pasiva"),
-    logo: "https://images.deliveryhero.io/image/pedidosya/restaurants/la-pasiva-logo.jpg?quality=70&width=100",
+    logo: "https://images.rappi.com.uy/restaurants_logo/lapasiva-centro-1581453375579.png?e=webp&q=80&d=200x200",
     background:
       "https://images.deliveryhero.io/image/pedidosya/header-backgrounds/mobile/pizza-4.jpg?quality=70&width=550&webp=1",
   },
@@ -27,9 +27,30 @@ const companies = [
   {
     name: "Mc Donald's",
     slug: sluggy("Mc Donald's"),
-    logo: "https://images.deliveryhero.io/image/pedidosya/restaurants/mcdonald-s-18-y-ejido-logo.jpg?quality=70&width=100&webp=1",
+    logo: "https://images.rappi.com.uy/restaurants_logo/111919-1543425311.png?e=webp&d=100x100&q=100",
     background:
       "https://images.deliveryhero.io/image/pedidosya/profile-headers/mc-donalds-uruguay-header-custom-2020.jpg?quality=70&width=1440&webp=1",
+  },
+  {
+    name: "Burger King",
+    slug: sluggy("Burger King"),
+    logo: "https://images.rappi.com.uy/restaurants_logo/nuevo-logo-bk-1610117467515.png?e=webp&d=5x5&q=100",
+    background:
+      "https://images.deliveryhero.io/image/pedidosya/profile-headers/burger-king-custom-header.jpg?quality=70&width=768&webp=1",
+  },
+  {
+    name: "Medialunas Calentitas",
+    slug: sluggy("Medialunas Calentitas"),
+    logo: "https://images.rappi.com.uy/restaurants_logo/mediaslunascalentitas-logo-1-1567609569925.png?e=webp&d=100x100&q=100",
+    background:
+      "https://images.rappi.com.uy/restaurants_background/uymedialunascalentitass-1666634475880.jpg?e=webp&d=700x100&q=100",
+  },
+  {
+    name: "Avenida Bar",
+    slug: sluggy("Avenida Bar"),
+    logo: "https://images.rappi.com.uy/restaurants_logo/avenidabar-1568314996192.png?e=webp&d=100x100&q=100",
+    background:
+      "https://images.rappi.com.uy/restaurants_background/uyavenidabar-1665777769766.jpg?e=webp&d=700x100&q=100",
   },
 ];
 
