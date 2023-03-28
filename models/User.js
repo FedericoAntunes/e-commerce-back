846cfe89fac3f861ff051b33d02a1e1a171c5ffb
Sebasan22 <callofsebas@hotmail.com>
2023-03-28 11:01:57 -0300

    back model

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const bcrypt = require("bcryptjs");
+const slugify = require("slugify");
 
 //const Role = require("./Role");
 
@@ -21,9 +22,15 @@ class User extends Model {
           type: DataTypes.STRING,
           allowNull: false,
         },
+        username: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+        },
         email: {
           type: DataTypes.STRING,
           allowNull: false,
+          unique: true,
         },
         password: {
           type: DataTypes.STRING,
@@ -37,6 +44,29 @@ class User extends Model {
           type: DataTypes.STRING,
         },
       },
+      // Email Slugify
+      /* User.beforeCreate((user) => {
+        user.email = user.email.replace(/[$%]/gi, "");
+        user.email = slugify(user.email, {
+          replacement: "-",
+          trim: true,
+          lower: true,
+          strict: false,
+        });
+      }),
+      // Hide Password in Store
+       (User.prototype.toJSON = function () {
+        const user = this.get();
+        delete user.password;
+        return user;
+      }),
+      // Bcrypt Password
+      User.beforeSave(async (user) => {
+        if (user.changed("password")) {
+          user.password = await bcrypt.hash(user.password, 8);
+        }
+      }),*/
+
       {
         sequelize,
         modelName: "user",
--- seeders/userSeeder.js
+++ seeders/userSeeder.js
@@ -4,18 +4,29 @@ const bcrypt = require("bcryptjs");
 module.exports = async () => {
   const users = [
     {
+      firstname: "user",
+      lastname: "user",
+      username: "user",
+      email: "user@hotmail.com",
+      password: await bcrypt.hash("123", 8),
+      address: { location: "Villa Dolores", tel: "9999999" },
+      avatar: "https://datepsychology.com/wp-content/uploads/2022/09/gigachad.jpg",
+    },
+    {
       firstname: "Seba",
       lastname: "Yaben",
+      username: "sebay",
       email: "seba@hotmail.com",
-      password: await bcrypt.hash("1234", 8),
+      password: await bcrypt.hash("123", 8),
       address: { location: "Villa Dolores", tel: "9999999" },
       avatar: "https://datepsychology.com/wp-content/uploads/2022/09/gigachad.jpg",
     },
     {
       firstname: "Cristiano",
       lastname: "Ronaldo",
+      username: "cristianito",
       email: "cris@hotmail.com",
-      password: await bcrypt.hash("1234", 8),
+      password: await bcrypt.hash("123", 8),
       address: { location: "Peñarol", tel: "555222333" },
       avatar: "https://www.fichajes.com/build/images/player-covers/cristiano-ronaldo.352c95f5.jpg",
     },
