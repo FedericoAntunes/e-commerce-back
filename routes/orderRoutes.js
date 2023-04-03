7cea265fc5d9ac97c046edd518a81c0f447524d6
Sebasan22 <callofsebas@hotmail.com>
2023-04-03 15:07:42 -0300

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const orderController = require("../controllers/orderController");
 const { expressjwt: checkJwt } = require("express-jwt");
 
+router.get("/", orderController.index);
 
 router.use(checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }));
-router.get("/", orderController.index);
 router.get("/:slug", orderController.show);
 router.post("/", orderController.store);
 
