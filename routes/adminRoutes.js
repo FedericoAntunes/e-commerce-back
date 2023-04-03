ea4262faa4eeb8187b99690f26db63c7a3cd4fec
Sebasan22 <callofsebas@hotmail.com>
2023-04-03 18:09:38 -0300

    admin only see admins index

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const adminController = require("../controllers/adminController");
 const { expressjwt: checkJwt } = require("express-jwt");
 
-router.get("/", adminController.index);
 // router.get("/crear", adminController.create);
 // router.get("/:id", adminController.show);
 // router.get("/editar/:id", adminController.edit);
 router.use(checkJwt({ secret: process.env.JWT_ADMIN_SECRET, algorithms: ["HS256"] }));
+router.get("/", adminController.index);
 router.post("/", adminController.store);
 //router.patch("/:id", adminController.update);
 //router.delete("/:id", adminController.destroy);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,9 +5,6 @@ const { expressjwt: checkJwt } = require("express-jwt");
 
 router.get("/", userController.index);
 router.post("/", userController.store);
-// router.get("/crear", userController.create);
-// router.get("/:id", userController.show);
-// router.get("/editar/:id", userController.edit);
 router.use(checkJwt({ secret: process.env.JWT_ADMIN_SECRET, algorithms: ["HS256"] }));
 router.patch("/:id", userController.update);
 router.delete("/:id", userController.destroy);
