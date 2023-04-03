057f39b3070e400dcccfbba2326b14eddd2b6faa
Sebasan22 <callofsebas@hotmail.com>
2023-04-03 15:15:57 -0300

    Update adminSeeder.js

--- seeders/adminSeeder.js
+++ seeders/adminSeeder.js
@@ -10,7 +10,7 @@ module.exports = async () => {
       email: "admin@hotmail.com",
       password: await bcrypt.hash("123", 8),
       address: { location: null, tel: null },
-      avatar: null,
+      avatar: "https://datepsychology.com/wp-content/uploads/2022/09/gigachad.jpg",
     },
   ];
   await Admin.bulkCreate(admins);
