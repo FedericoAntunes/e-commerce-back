0c27c0ea536337f14b5bc40f2394e2676e5b2c14
Sebasan22 <callofsebas@hotmail.com>
2023-04-03 18:11:42 -0300

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,9 +3,6 @@ const router = express.Router();
 const adminController = require("../controllers/adminController");
 const { expressjwt: checkJwt } = require("express-jwt");
 
-// router.get("/crear", adminController.create);
-// router.get("/:id", adminController.show);
-// router.get("/editar/:id", adminController.edit);
 router.use(checkJwt({ secret: process.env.JWT_ADMIN_SECRET, algorithms: ["HS256"] }));
 router.get("/", adminController.index);
 router.post("/", adminController.store);
