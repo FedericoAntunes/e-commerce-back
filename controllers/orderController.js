7196367b42add85172e70cdcd0ccaa3cc93564b3
Sebasan22 <callofsebas@hotmail.com>
2023-04-12 10:57:17 -0300

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -17,42 +17,60 @@ async function show(req, res) {
 // Store a newly created resource in storage.
 async function store(req, res) {
   const { shippingData, total_price } = req.body;
+
   const { products, address, payment_method, payment_info } = shippingData;
   let total = 0;
   for (const product of products) {
     total += product.price * product.quantity;
   }
+
   if (
-    Math.abs(total + total * Number(process.env.TAX) + Number(process.env.SHIPPING) - total_price) <
-    0.02
+    address.firstname === "" ||
+    address.address === "" ||
+    address.lastname === "" ||
+    address.city === "" ||
+    address.province === "" ||
+    address.postal_code === "" ||
+    payment_info.card_number === "" ||
+    payment_info.name_card === "" ||
+    payment_info.expiration_date === "" ||
+    payment_info.cvv === ""
   ) {
-    const order = await Order.create({
-      status: "In process",
-      address,
-      total_price,
-      payment_method,
-      payment_info,
-      userId: req.auth.id,
-    });
-    const orderProducts = [];
-    for (const product of products) {
-      await Product.update(
-        { stock: product.stock - product.quantity },
-        { where: { id: product.id } },
-      );
-      const orderProduct = {
-        qty: product.quantity,
-        unit_price: product.price,
-        product_name: product.title,
-        orderId: order.id,
-        productId: product.id,
-        image: product.image,
-      };
-      orderProducts.push(orderProduct);
-      await OrderProduct.create(orderProduct);
+    return res.status(402).json("Fill all fields.");
+  } else {
+    if (
+      Math.abs(
+        total + total * Number(process.env.TAX) + Number(process.env.SHIPPING) - total_price,
+      ) < 0.02
+    ) {
+      const order = await Order.create({
+        status: "In process",
+        address,
+        total_price,
+        payment_method,
+        payment_info,
+        userId: req.auth.id,
+      });
+      const orderProducts = [];
+      for (const product of products) {
+        await Product.update(
+          { stock: product.stock - product.quantity },
+          { where: { id: product.id } },
+        );
+        const orderProduct = {
+          qty: product.quantity,
+          unit_price: product.price,
+          product_name: product.title,
+          orderId: order.id,
+          productId: product.id,
+          image: product.image,
+        };
+        orderProducts.push(orderProduct);
+        await OrderProduct.create(orderProduct);
+      }
+      order.dataValues.products = orderProducts;
+      return res.status(201).json(order);
     }
-    order.dataValues.products = orderProducts;
-    return res.status(201).json(order);
   }
   return res.status(406).json({ Error: "Error" });
 }
